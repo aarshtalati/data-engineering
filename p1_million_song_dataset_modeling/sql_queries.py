49fe506b461a31f57fee1af5e600d29864de1e88
Aarsh Talati <aarshstalati1989@gmail.com>
2021-01-14 20:33:03 -0500

    rename p1 dir, fix SQL query

--- p1_million_song_dataset_modeling/sql_queries.py
+++ p1_million_song_dataset_modeling/sql_queries.py
@@ -71,7 +71,7 @@ INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_i
 """)
 
 user_table_insert = ("""
-INSERT INTO users (user_id, first_name, last_name, gender) VALUES (%s, %s, %s, %s)
+INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s)
 """)
 
 song_table_insert = ("""
